37aaac929d6e4b9960183e3f6220eaeb5006b73d
d.suharev <solaris@bbq.agency>
2023-03-15 10:01:32 +0200

    add new pages for test

--- server/backstop/config/scenarios/alpa.ts
+++ server/backstop/config/scenarios/alpa.ts
@@ -3,42 +3,109 @@ export default function getScenarios(hostName: string) {
         {
             label: "Homepage",
             url: `http://${ hostName }/`,
+            delay: 3500,
+            readySelector: ".jackpot-item",
+        },
+        {
+            label: "Homepage auth",
+            url: `http://${ hostName }/`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+            readySelector: ".jackpot-item",
+        },
+        // {
+        //     label: "Deposit",
+        //     url: `http://${ hostName }/`,
+        //     cookiePath: "backstop/data/engine_scripts/cookies.json",
+        //     clickSelector: "[data-test=\"header__deposit-btn\"]",
+        //     delay: 3500,
+        // },
+        {
+            label: "Profile",
+            url: `http://${ hostName }/users/`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+        },
+        {
+            label: "Profile level",
+            url: `http://${ hostName }/users/level`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+        },
+        {
+            label: "Profile gifts",
+            url: `http://${ hostName }/users/gifts`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+        },
+        {
+            label: "Profile verification",
+            url: `http://${ hostName }/users/verification`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+        },
+        {
+            label: "Profile limits",
+            url: `http://${ hostName }/users/limits`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+        },
+        {
+            label: "Profile security",
+            url: `http://${ hostName }/users/security`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
+        },
+        {
+            label: "Profile games-history",
+            url: `http://${ hostName }/users/games-history`,
+            cookiePath: "backstop/data/engine_scripts/cookies.json",
+            delay: 3500,
         },
         {
             label: "Login",
             url: `http://${ hostName }/login`,
+            delay: 3500,
         },
         {
             label: "Registration",
             url: `http://${ hostName }/registration`,
+            delay: 3500,
         },
         {
             label: "terms-and-conditions",
             url: `http://${ hostName }/terms-and-conditions`,
+            delay: 3500,
         },
         {
             label: "vip",
             url: `http://${ hostName }/vip`,
+            delay: 3500,
         },
         {
             label: "benefits-of-crypto",
             url: `http://${ hostName }/benefits-of-crypto`,
+            delay: 3500,
         },
         {
             label: "faq",
             url: `http://${ hostName }/faq`,
+            delay: 3500,
         },
         {
             label: "support",
             url: `http://${ hostName }/support`,
+            delay: 3500,
         },
         {
             label: "sport/registration",
             url: `http://${ hostName }/sport/registration`,
+            delay: 3500,
         },
         {
             label: "sport/login",
             url: `http://${ hostName }/sport/login`,
+            delay: 3500,
         },
     ];
 }
