85ede11ed3a710196fca2a5a1048ea341b1df07c
Dmitriy Suharev <solaris@upstars.com>
2023-07-17 14:02:52 +0000

    Update gitlabci & slack

--- server/services/SlackService.ts
+++ server/services/SlackService.ts
@@ -1,6 +1,20 @@
 import axios from "axios";
 
-const chanelHook = "https://hooks.slack.com/services/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";
+function getChanelHook(project: string): string {
+    if (project === 'alpa') {
+        return "https://hooks.slack.com/services/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"
+    }
+
+    return "https://hooks.slack.com/services/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"
+};
+
+function getText(project: string, testId: string): string {
+    if (project === 'alpa') {
+        return `Test new tag <https://gitlab.upstr.to/whitelabel/frontera/-/tags/${ testId }|${ testId }> ended with errors`
+    }
+
+    return `Test new release "${ testId }" ended with errors`
+}
 
 export default {
     send: function send(project: string, testId: string) {
@@ -8,18 +22,14 @@ export default {
             return;
         }
 
-        if (project !== "alpa") {
-            return;
-        }
-
-        axios.post(chanelHook, {
+        axios.post(getChanelHook(project), {
             "text": `Test ${ testId } ended with errors`,
             "pretty": 1,
             "blocks": [
                 {
                     "type": "section",
                     "text": {
-                        "text": `Test new tag <https://gitlab.bbq.agency/bbq/frontera/-/tags/${ testId }|${ testId }> ended with errors`,
+                        "text": getText(project, testId),
                         "type": "mrkdwn"
                     }
                 },
@@ -33,7 +43,7 @@ export default {
                                 "text": "Result"
                             },
                             "style": "primary",
-                            "url": `https://toledo-staging.upstr.to/report/alpa/${ testId }`
+                            "url": `https://toledo-staging.upstr.to/report/${ project }/${ testId }`
                         }
                     ]
                 }
