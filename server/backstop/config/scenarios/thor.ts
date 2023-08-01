cbe60ee93931a2965d835eb1dfaeb858f59a2a0f
d.suharev <solaris@bbq.agency>
2023-08-01 19:10:07 +0300

    up

--- server/backstop/config/scenarios/thor.ts
+++ server/backstop/config/scenarios/thor.ts
@@ -1,12 +1,54 @@
 import { processHost } from '../../../helpers/cookiesHelper'
 import {MOCK_ADDR}from '@config'
 export default function getScenarios(host: string) {
-  const cookiePath = MOCK_ADDR ? '' : processHost(host)
+  const cookiePath = MOCK_ADDR ? 'backstop/config/cookies/cookies-thor.json' : processHost(host)
 
   return [
     {
       label: 'Homepage',
       url: `${host}/`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Casino',
+      url: `${host}/casino`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Live',
+      url: `${host}/live`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Arena',
+      url: `${host}/arena`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Quests',
+      url: `${host}/quests`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Bonus',
+      url: `${host}/bonus`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Bonus sport',
+      url: `${host}/bonus/sport`,
+      delay: 3000,
+      readySelector: '.header__wrapper'
+    },
+    {
+      label: 'Vip',
+      url: `${host}/vip`,
       delay: 3000,
       readySelector: '.header__wrapper'
     },
