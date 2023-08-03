faf7502fff5e7c8afb46e9bda1a02f8301e2fd4d
d.suharev <solaris@bbq.agency>
2023-08-03 17:11:11 +0300

    up

--- server/backstop/data/engine_scripts/puppet/onBefore.js
+++ server/backstop/data/engine_scripts/puppet/onBefore.js
@@ -1,3 +1,7 @@
 module.exports = async (page, scenario, vp) => {
   await require('./loadCookies')(page, scenario);
+
+  if (vp.label === 'mobile') {
+    page.setUserAgent('Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)')
+  }
 };
