7d64c6bc627f734c21f1b1908714dc29c8d164db
d.suharev <solaris@bbq.agency>
2023-08-03 18:20:53 +0300

    up

--- server/backstop/data/engine_scripts/puppet/onReady.js
+++ server/backstop/data/engine_scripts/puppet/onReady.js
@@ -1,7 +1,7 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./overrideCSS')(page, scenario);
-  await require('./clickAndHoverHelper')(page, scenario);
+  await require('./clickAndHoverHelper')(page, scenario, vp);
 
   // add more ready handlers here...
 };
