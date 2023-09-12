c60352d981556290059787fc326308575e17644e
d.tashchi <MBnjB376bbV9vb>
2023-09-12 10:47:25 +0300

    ALPA-2012: - try to stable profile tests; - add vip adventure page for test

--- server/helpers/cookiesHelper.ts
+++ server/helpers/cookiesHelper.ts
@@ -7,9 +7,9 @@ export function processHost(host: string): string {
     const file = fs.readFileSync(path.resolve(__dirname, pasePath, 'default.json'), 'utf8')
     const parsedFile = JSON.parse(file)
     parsedFile[0].domain = domain
-    parsedFile[0].name = domain.indexOf('alpa') > -1 ? '_casino_session' : 'default_token'
-    const fileName = `cookies-${ domain.replace('http://', '').replace(/\./g, '-') }.json`
+    parsedFile[0].name = /rocketplay|alpa/.test(domain) ? '_casino_session' : 'default_token'
+    const fileName = `cookies-${domain.replace('http://', '').replace(/\./g, '-')}.json`
     fs.writeFileSync(path.resolve(__dirname, pasePath, fileName), JSON.stringify(parsedFile));
 
-    return `backstop/config/cookies/${ fileName }`
+    return `backstop/config/cookies/${fileName}`
 }
