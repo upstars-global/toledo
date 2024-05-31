3bb08b8286948211b60a23867e84d6ef623870a5
IvanKhartov <ivan.khartov@gmail.com>
2024-05-31 11:30:50 +0300

    FP-1712: comment wrong url for thor to unblock releases

--- server/helpers/hostHelper.ts
+++ server/helpers/hostHelper.ts
@@ -14,9 +14,9 @@ function getCookieUrlByTask(config: IHost): string {
         return `frontera${ task }-mock.${ config.project }.svc.cluster.local`;
     }
 
-    if (config.project === 'thor') {
-        return `frontera${ task }-${ config.project }-${ env }-mock.thor-frontera.svc.cluster.local`;
-    }
+    // if (config.project === 'thor') {
+    //     return `frontera${ task }-${ config.project }-${ env }-mock.thor-frontera.svc.cluster.local`;
+    // }
 
     return `frontera${ task }-${ config.project }-${ env }-mock.${ config.project }.svc.cluster.local`;
 }
