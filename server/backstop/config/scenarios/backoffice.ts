c8fda13ed977895f6c2bb69122c4293a46d1f3ea
@solaris <solaris@upstars.com>
2025-01-30 10:01:16 +0200

    40

--- server/backstop/config/scenarios/backoffice.ts
+++
@@ -1,9 +0,0 @@
-export default function getScenarios(host: string) {
-    return [
-        {
-            label: 'Homepage',
-            url: `${host}/`,
-            delay: 3000,
-        },
-    ];
-}
