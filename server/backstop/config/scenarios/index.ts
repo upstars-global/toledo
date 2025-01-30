f5c4f429b07b140fe3a86889d94cd9c71f4614a5
@solaris <solaris@upstars.com>
2025-01-30 10:10:46 +0200

    41

--- server/backstop/config/scenarios/index.ts
+++ server/backstop/config/scenarios/index.ts
@@ -1,10 +1,8 @@
 import alpa from './alpa';
-import thor from './thor';
 import { PROJECT } from "@config";
 
 const configs: Record<string, any> = {
     alpa,
-    thor,
 };
 
 export default function getScenarios() {
