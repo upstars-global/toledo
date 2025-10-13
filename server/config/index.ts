2544e4a9baabc0988a402c0031acd67e093444ce
ikhartov <i.khartov@upstars.com>
2025-10-13 14:45:07 +0300

    FP-3795: Add `REPO_URL` and enhance Slack notifications with improved task/env handling
    - Introduced `REPO_URL` to configurations (`values.yaml` and `index.ts`).
    - Updated Slack notification logic to include environment details and handle URLs dynamically using `REPO_URL`.
    - Incremented Helm chart version to 1.6.0.

--- server/config/index.ts
+++ server/config/index.ts
@@ -5,4 +5,5 @@ export const DEFAULT_SERVICE = process.env.DEFAULT_SERVICE;
 export const SLACK_CHANEL = process.env.SLACK_CHANEL;
 export const CLIENT_ADDR = process.env.CLIENT_ADDR;
 export const MOCK_ADDR = process.env.MOCK_ADDR;
+export const REPO_URL = process.env.REPO_URL;
 export const NODE_ENV = process.env.NODE_ENV;
