bf392519d2c81b9204cde68f375b349f4e60f0e6
Artem Oleinychenko <a.oleinychenko@upstars.com>
2026-09-07 19:10:30 +0300

    UN-3160 toledo improvements / hooks to replace delay

--- server/backstop/data/engine_scripts/puppet/onReady.js
+++ server/backstop/data/engine_scripts/puppet/onReady.js
@@ -1,9 +1,11 @@
-module.exports = async (page, scenario, vp) => {
-  console.log('SCENARIO > ' + scenario.label)
-  await require('./overrideCSS')(page, scenario)
+module.exports = async (page, scenario, vp, ...backstopArgs) => {
+  const [, , config] = backstopArgs;
+
+  console.log('SCENARIO > ' + scenario.label);
+  await require('./overrideCSS')(page, scenario);
   // await require('./paymentsLibSS')(page) commit for king
 
-  await require('./clickAndHoverHelper')(page, scenario, vp)
+  await require('./clickAndHoverHelper')(page, scenario, vp, config);
 
   // add more ready handlers here...
-}
+};
